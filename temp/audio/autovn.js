2d53f38674e825ba08679c00dbce7b305a02820a
fariz-ind <78103656+fariz-ind@users.noreply.github.com>
2024-08-16 06:45:21 +0700

    Add files via upload

---
+++ temp/audio/autovn.js
@@ -0,0 +1,54 @@
+//.VN MILIK NINA KAWAI AHH YMATE KUDASAI
+
+//KETIKA ADA YANG TOXIC
+exports.vnToxic = [
+//Dosa pantek
+"https://cdn.filestackcontent.com/zdj4WDdSAiy9SpbTTZj7",
+//Jangan toxic pm
+"https://cdn.filestackcontent.com/Zdfunz5gQLeTCF4DlQOW",
+//Heeh
+"https://cdn.filestackcontent.com/YYG9wdIgRLSHi5Or45dO",
+//goblok
+"https://cdn.filestackcontent.com/An4uBvGOQ5iuQWAJqlp5",
+//paansih
+"https://cdn.filestackcontent.com/lYHaYLVNS4G7QSagNEvX"
+]
+exports.vnMenu = [
+"https://cdn.filestackcontent.com/H7X7obIgTjG2evHhcorI",
+"https://cdn.filestackcontent.com/9PXbmZOjRMKrT2I4oSGk",
+ "https://cdn.filestackcontent.com/huJaFQIxRVyFEyeDQ1F3"
+]
+exports.vnOwner = [ 
+//gaboleh
+"https://cdn.filestackcontent.com/u6ivfrjHSiqifCvnCvEA",
+ //gamau
+   "https://cdn.filestackcontent.com/xIuf4mJ9RkSGJAXye9JA",
+ //gaada
+ "https://cdn.filestackcontent.com/gOGmQMpkQWG7iQcaM6Cd"
+]
+exports.vnAra = [ 
+   "https://cdn.filestackcontent.com/w7FUUZRnKnBbW3JBPXgw",
+// aragoblok
+"https://cdn.filestackcontent.com/3A8QclaSMSoruULZsb6w"
+    ]
+exports.vnBot = [ 
+   "https://cdn.filestackcontent.com/u6YY3T2WRhCx9wgHd84y",
+// oy
+"https://cdn.filestackcontent.com/aQhw3WDBQHC6TOn1J6FF",
+// iyakak
+"https://cdn.filestackcontent.com/z9jSPfkJRA2wZefh9Exg",
+// kenapa kak
+"https://cdn.filestackcontent.com/3Mcm018pRW7gYot1Nv5I"
+    ]
+exports.vnSalam = [ "https://cdn.filestackcontent.com/CCN0AzPQhuqaG9zXRrFg"]
+exports.vnSpam = [ "https://cdn.filestackcontent.com/wlGqcb1RVOT9YOTzI9kz"]
+exports.vnPagi = [ "https://cdn.filestackcontent.com/5B3I5N2jQ2WJCr8lblNv",
+ "https://cdn.filestackcontent.com/BdeSoIRwTE65TC08Gz3k"             ]
+exports.vnSiang = [ 
+ //koniciwa
+ "https://cdn.filestackcontent.com/X5S3BPlkQGCDjUmDnuWn" ]
+exports.vnMalam = [ "https://cdn.filestackcontent.com/TbQKutSEKhzuQW75Eigg" ]
+
+exports.vnKawai = [ "https://cdn.filestackcontent.com/zHMFviWeT6eDOvu3eJe3"]
+exports.vnLove = [ "https://cdn.filestackcontent.com/kaZNTOQRUiKO1Ve91KO7",
+"https://cdn.filestackcontent.com/Qm07O9xxQQygvAlBhzHb"            ]
